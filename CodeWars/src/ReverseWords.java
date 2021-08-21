a451b491e042570a24c1ed5c532c8f837c0cfa29
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-08-21 11:34:22 -0300

    Code Wars - SortAndStar

--- CodeWars/src/ReverseWords.java
+++ CodeWars/src/ReverseWords.java
@@ -4,7 +4,7 @@ public class reverseWords {
 
   public static String reverseWords(final String original) {
     String[] array = original.split(" ");
-    if (array.length == 0) return original;
+    if (array.length == 0) return original; // case  expected:<[   ]> but was:<[]>
     int i = 0;
     for (String item : array) {
       array[i] = new StringBuilder(item).reverse().toString();
