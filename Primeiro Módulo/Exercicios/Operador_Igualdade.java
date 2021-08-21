8c9086edb74af28eb825a5642665e272c5ade6d0
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-08-21 11:46:11 -0300

    Operador_Igualdade.java

---
+++ Primeiro Módulo/Exercicios/Operador_Igualdade.java
@@ -0,0 +1,9 @@
+public class Operador_Igualdade {
+
+  public static void main(String[] args) {
+    int idade = 16;
+    if (idade <= 16) {
+      System.out.println("É menor de idade");
+    }
+  }
+}
