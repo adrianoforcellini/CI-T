22a530dee1edd4d7806f682f8adfc916e75c633c
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-08-21 11:47:30 -0300

    Exercicios - Valor_Menor

---
+++ Primeiro Módulo/Exercicios/Valor_Menor.java
@@ -0,0 +1,8 @@
+public class Valor_Menor {
+
+  public static void main(String[] args) {
+    int valorA = 14;
+    int valorB = 20;
+    if (valorA < valorB) System.out.printf("%d é menor que %d", valorA, valorB);
+  }
+}
