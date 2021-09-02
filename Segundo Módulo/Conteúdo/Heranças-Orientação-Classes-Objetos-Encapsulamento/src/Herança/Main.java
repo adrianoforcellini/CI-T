abb5586aeb5ffffb4337d8305323d782aeaa4ebf
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-09-01 22:03:28 -0300

    Adicionando elemento que não é herança á classe instanciada

--- Segundo Módulo/Conteúdo/Heranças-Orientação-Classes-Objetos-Encapsulamento/src/Herança/Main.java
+++ Segundo Módulo/Conteúdo/Heranças-Orientação-Classes-Objetos-Encapsulamento/src/Herança/Main.java
@@ -5,10 +5,12 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         Aluno Anderson = new Aluno("Anderson Francisco", "123.456.789-00", new Date());
+        Anderson.matricula = "123564";
         System.out.println("Veja como os atributos foram preenchidos\n\nAluno:\nNome: " + Anderson.nome);
         System.out.println("CPF: " + Anderson.cpf);
         System.out.println("Data de nascimento: " + Anderson.data_nascimento.toString());
         System.out.println("O Aluno pagará " + Math.round(Anderson.tirarCopias(100)) + " em cada 100 cópias.");
+        System.out.println("A matricula de Anderson é:"+ Anderson.matricula);
 
         Funcionario Emerson = new Funcionario("Emerson Soares", "714.369.789-00", new Date());
         System.out.println("\n\nFuncionario:\nNome: " + Emerson.nome);
