746b50d3addd94f69008018ddc74bb071ea904c6
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-09-01 21:16:18 -0300

    alterando as classes Pssoa e Aluno para adicionar o calculo do preço de copias

--- Segundo Módulo/Conteúdo/Heranças-Orientação-Classes-Objetos-Encapsulamento/src/Herança/Aluno.java
+++ Segundo Módulo/Conteúdo/Heranças-Orientação-Classes-Objetos-Encapsulamento/src/Herança/Aluno.java
@@ -1,9 +1,13 @@
 package Herança;
 
 import java.util.Date;
+
 public class Aluno extends Pessoa {
     public Aluno(String _nome, String _cpf, Date _data) {
         super(_nome, _cpf, _data);
     }
     public String matricula;
+    public double tirarCopias(int qtd) { //Preço para tirar copias para alunos
+        return 0.07 * (double) qtd;
+    }
 }
