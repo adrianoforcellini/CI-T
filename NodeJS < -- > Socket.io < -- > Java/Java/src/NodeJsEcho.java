c69fb2611cb55756cc89e90c6005a5fe1d9336a2
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-09-07 10:57:59 -0300

    Criada a conexao, e adaptada para as respostas fazerem sentido

---
+++ NodeJS < -- > Socket.io < -- > Java/Java/src/NodeJsEcho.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+/**
+ * Classe Java (client) para Comunicação com servidor NodeJS
+ * Escreve e recebe um simples "echo" -> "mensagem123"
+ * by Douglas.Pasqua
+ * http://douglaspasqua.com
+ */
+public class NodeJsEcho {
+    // objeto socket
+    private Socket socket = null;
+
+    public static void main(String[] args) throws UnknownHostException,
+            IOException, ClassNotFoundException {
+        // instancia classe
+        NodeJsEcho client = new NodeJsEcho();
+
+        // conexão socket tcp
+        String ip = "127.0.0.1";
+        int port = 6969;
+        client.socketConnect(ip, port);
+
+        // escreve e recebe mensagem
+        String message = "Olá Node!!!!";
+
+        System.out.println("Enviando: " + message);
+        String retorno = client.echo(message);
+        System.out.println("Recebendo: " + retorno);
+    }
+
+    // realiza a conexão com o socket
+    private void socketConnect(String ip, int port) throws UnknownHostException, IOException {
+        System.out.println("[Conectando socket...]");
+        this.socket = new Socket(ip, port);
+    }
+
+    // escreve e recebe mensagem full no socket (String)
+    public String echo(String message) {
+        try {
+            // out & in
+            PrintWriter out = new PrintWriter(getSocket().getOutputStream(), true);
+            BufferedReader in = new BufferedReader
+                    (new InputStreamReader(getSocket().getInputStream()));
+
+            // escreve str no socket e lêr
+            out.println("!");
+            String retorno = in.readLine();
+            return retorno;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    // obtem instância do socket
+    private Socket getSocket() {
+        return socket;
+    }
+}
---
+++ NodeJS < -- > Socket.io < -- > Java/NodeJS/server.js
@@ -0,0 +1,27 @@
+const net = require('net');
+
+const HOST = '127.0.0.1'; // parametrizar o IP do Listen
+const PORT = 6969; // porta TCP LISTEN
+
+// Cria a instância do Server e aguarda uma conexão
+net.createServer(function(sock) {
+
+    // Opa, recebemos uma conexão - um objeto socket é associado à conexão automaticamente
+    console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
+
+    // Adiciona um 'data' - "event handler" nesta instância do socket
+    sock.on('data', function(data) {
+        // dados foram recebidos no socket
+        // Escreve a mensagem recebida de volta para o socket (echo)
+        sock.write(`Oláá, Javaaa!!!${data}`);
+    });
+
+    // Adiciona um 'close' - "event handler" nesta instância do socket
+    sock.on('close', function(data) {
+        // conexão fechada
+        console.log('CLOSED: ' + sock.remoteAddress +' '+ sock.remotePort);
+    });
+
+}).listen(PORT, HOST);
+
+console.log('Server listening on ' + HOST +':'+ PORT);
